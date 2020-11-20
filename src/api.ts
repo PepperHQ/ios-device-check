4f6813694f52f4c003bf6f02d3ccb673d5fcfaa2
Dominic Rathbone <6716654+dominicrathbone@users.noreply.github.com>
2020-11-20 15:08:51 +0000

    store corresponding request and response in thrown errors

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,12 @@
 // (c) PepperHQ Limited - All Right Reserved
-import fetch from 'node-fetch';
+import fetch, { Request, Response } from 'node-fetch';
 import uuid from 'uuid';
+
+export class ApiError extends Error {
+    constructor(public req: Request, public res: Response | undefined, public wrappedError: Error) {
+        super(wrappedError.message);
+    }
+}
 
 // API Documentation available at:
 // https://developer.apple.com/documentation/devicecheck/accessing_and_modifying_per-device_data
@@ -26,38 +32,48 @@ export interface QueryDeviceResult {
 
 function isQueryDeviceResult(result: any): result is QueryDeviceResult {
     return typeof result.bit0 === 'boolean' &&
-        typeof result.bit1 === 'boolean' && 
+        typeof result.bit1 === 'boolean' &&
         typeof result.last_update_time === 'string';
 }
 
 export async function QueryDevice(host: string, jwt: string, payload: QueryDevicePayload): Promise<QueryDeviceResult> {
-    const url = `https://${host}/v1/query_two_bits`;
+    let request;
+    let response;
+    try {
+        const url = `https://${host}/v1/query_two_bits`;
 
-    const headers = {
-        Authorization: `Bearer ${jwt}`,
-        'Content-Type':'application/json'
+        const headers = {
+            Authorization: `Bearer ${jwt}`,
+            'Content-Type': 'application/json'
+        }
+
+        const body: QueryDevicePayload = {
+            device_token: payload.device_token,
+            transaction_id: payload.transaction_id || uuid(),
+            timestamp: payload.timestamp || Date.now()
+        };
+
+        request = new Request(url, { method: 'POST', headers, body: JSON.stringify(body) })
+        response = await fetch(request);
+
+        if (response.status !== 200) {
+            throw new Error(`Device check api returned ${response.status}: ${await response.text()}`);
+        }
+
+        const responseBody = await response.json();
+
+        if (!isQueryDeviceResult(responseBody)) {
+            throw new Error('Failed to parse response from device check api');
+        }
+
+        return responseBody;
+    } catch (err) {
+        if (request) {
+            throw new ApiError(request, response?.clone(), err)
+        }
+        throw err
     }
-
-    const body: QueryDevicePayload = {
-        device_token: payload.device_token,
-        transaction_id: payload.transaction_id || uuid(),
-        timestamp: payload.timestamp || Date.now()
-    };
-
-    const response = await fetch(url, { method: 'POST', headers, body: JSON.stringify(body) });
-
-    if (response.status !== 200) {
-        throw new Error(`Device check api returned ${response.status}: ${await response.text()}`);
-    }
-
-    const responseBody = await response.json();
-
-    if (!isQueryDeviceResult(responseBody)) {
-        throw new Error('Failed to parse response from device check api');
-    }
-
-    return responseBody;
-} 
+}
 
 export interface UpdateDevicePayload extends QueryDevicePayload {
     bit0: boolean;
@@ -65,49 +81,69 @@ export interface UpdateDevicePayload extends QueryDevicePayload {
 }
 
 export async function UpdateDevice(host: string, jwt: string, payload: UpdateDevicePayload): Promise<void> {
-    const url = `https://${host}/v1/update_two_bits`;
+    let request;
+    let response;
+    try {
+        const url = `https://${host}/v1/update_two_bits`;
 
-    const headers = {
-        Authorization: `Bearer ${jwt}`,
-        'Content-Type':'application/json'
+        const headers = {
+            Authorization: `Bearer ${jwt}`,
+            'Content-Type': 'application/json'
+        }
+
+        const body: UpdateDevicePayload = {
+            device_token: payload.device_token,
+            bit0: payload.bit0,
+            bit1: payload.bit1,
+            transaction_id: payload.transaction_id || uuid(),
+            timestamp: payload.timestamp || Date.now(),
+        };
+
+        request = new Request(url, { method: 'POST', headers, body: JSON.stringify(body) })
+        response = await fetch(request);
+
+        if (response.status !== 200) {
+            throw new Error(`Device check api returned ${response.status}: ${await response.text()}`);
+        }
+    } catch (err) {
+        if (request) {
+            throw new ApiError(request, response?.clone(), err)
+        }
+        throw err
     }
-
-    const body: UpdateDevicePayload = {
-        device_token: payload.device_token,
-        bit0: payload.bit0,
-        bit1: payload.bit1,
-        transaction_id: payload.transaction_id || uuid(),
-        timestamp: payload.timestamp || Date.now(),
-    };
-
-    const response = await fetch(url, { method: 'POST', headers, body: JSON.stringify(body) });
-
-    if (response.status !== 200) {
-        throw new Error(`Device check api returned ${response.status}: ${await response.text()}`);
-    }
-} 
+}
 
 export type ValidateDevicePayload = QueryDevicePayload;
 
 // The documentation is cryptic about what is returned if the token is invalid so we assume any error === invalid 
 // and throw an error if one is returned instead of returning a boolean.
 export async function ValidateDevice(host: string, jwt: string, payload: ValidateDevicePayload): Promise<void> {
-    const url = `https://${host}/v1/validate_device_token`;
+    let request;
+    let response;
+    try {
+        const url = `https://${host}/v1/validate_device_token`;
 
-    const headers = {
-        Authorization: `Bearer ${jwt}`,
-        'Content-Type':'application/json'
-    }
+        const headers = {
+            Authorization: `Bearer ${jwt}`,
+            'Content-Type': 'application/json'
+        }
 
-    const body: ValidateDevicePayload = {
-        device_token: payload.device_token,
-        transaction_id: payload.transaction_id || uuid(),
-        timestamp: payload.timestamp || Date.now()
-    };
+        const body: ValidateDevicePayload = {
+            device_token: payload.device_token,
+            transaction_id: payload.transaction_id || uuid(),
+            timestamp: payload.timestamp || Date.now()
+        };
 
-    const response = await fetch(url, { method: 'POST', headers, body: JSON.stringify(body) });
+        request = new Request(url, { method: 'POST', headers, body: JSON.stringify(body) })
+        response = await fetch(request);
 
-    if (response.status !== 200) {
-        throw new Error(`Device check api returned ${response.status}: ${await response.text()}`);
+        if (response.status !== 200) {
+            throw new Error(`Device check api returned ${response.status}: ${await response.text()}`);
+        }
+    } catch (err) {
+        if (request) {
+            throw new ApiError(request, response?.clone(), err)
+        }
+        throw err
     }
 } 
